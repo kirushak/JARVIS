52f3a6b9e3e01d539e3cd8ab678d00cd3854b14e
Saket Gudimella <106355242+SaketGudimella@users.noreply.github.com>
2022-12-11 10:44:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import webbrowser
 import os
 import time
 import subprocess
-from ecapture import ecapture as ec
 import wolframalpha
-import json
 import requests
 import pywhatkit
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -34,7 +33,7 @@ def wishme():
     else:
         speak("Good Evening!")
 
-    speak("sir, how can i help you")
+    speak("Sir, how can i help you")
 
 def takeCommand():
 
@@ -71,51 +70,76 @@ if __name__=='__main__':
             break
 
             
-            ## Code for opening local applications on host's device (Saket)
+            ## Code for opening local applications on host's device (Saket) - Pending
 
- elif 'ip address' in query:
-            ip_address = find_my_ip()
-            speak(f'Your IP Address is {ip_address}.\n For your convenience, I am printing it on the screen sir.')
-            print(f'Your IP Address is {ip_address}')
-
-        elif 'wikipedia' in query:
-            speak('What do you want to search on Wikipedia, sir?')
-            search_query = take_user_input().lower()
-            results = search_on_wikipedia(search_query)
-            speak(f"According to Wikipedia, {results}")
-            speak("For your convenience, I am printing it on the screen sir.")
+            
+            
+            
+            if 'wikipedia' in statement:
+            speak('Searching Wikipedia...')
+            statement =statement.replace("wikipedia", "")
+            results = wikipedia.summary(statement, sentences=3)
+            speak("According to Wikipedia")
             print(results)
+            speak(results)
 
-        elif 'youtube' in query:
-            speak('What do you want to play on Youtube, sir?')
-            video = take_user_input().lower()
-            play_on_youtube(video)
+        elif 'open youtube' in statement:
+            webbrowser.open_new_tab("https://www.youtube.com")
+            speak("Opening Youtube....")
+            time.sleep(5)
 
-        elif 'search on google' in query:
-            speak('What do you want to search on Google, sir?')
-            query = take_user_input().lower()
-            search_on_google(query)
+        elif 'open google' in statement:
+            webbrowser.open_new_tab("https://www.google.com")
+            speak("Opening Google Chrome...")
+            time.sleep(5)
 
-        elif "trending movies" in query:
-            speak(f"Some of the trending movies are: {get_trending_movies()}")
-            speak("For your convenience, I am printing it on the screen sir.")
-            print(*get_trending_movies(), sep='\n')
+        elif 'open gmail' in statement:
+            webbrowser.open_new_tab("gmail.com")
+            speak("Opening Gmail...")
+            time.sleep(5)
+      
 
-        elif 'news' in query:
-            speak(f"I'm reading out the latest news headlines, sir")
-            speak(get_latest_news())
-            speak("For your convenience, I am printing it on the screen sir.")
-            print(*get_latest_news(), sep='\n')
+        elif "open facebook" in statement:
+            webbrowser.open_new_tab("https://facebook.com/login")
+            speak("Opening Facebook....")
+   
+        elif "open instagram" in statement:
+            webbrowser.open_new_tab("https://instagram.com/login")
+            speak("Opening Instagram....")
+            
+          elif "open twitter" in statement:
+            webbrowser.open_new_tab("https://twitter.com/login")
+            speak("Opening Twitter....")
+            
+            
+            elif "open github" in statement:
+            webbrowser.open_new_tab("https://github.com/login")
+            speak("Opening Github....")
+           
+         
+          elif 'search'  in statement:
+            statement = statement.replace("search", "")
+            webbrowser.open_new_tab(statement)
+            time.sleep(5)
+            
+           
+        elif 'time' in statement:
+            strTime=datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"the time is {strTime}")
+            
+            
+        elif 'news' in statement:
+            news = webbrowser.open_new_tab("https://ndtv.com")
+            speak('Here are some latest headlines')
+            time.sleep(6)
 
-        elif 'weather' in query:
-            ip_address = find_my_ip()
-            city = requests.get(f"https://ipapi.co/{ip_address}/city/").text
-            speak(f"Getting weather report for your city {city}")
-            weather, temperature, feels_like = get_weather_report(city)
-            speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
-            speak(f"Also, the weather report talks about {weather}")
-            speak("For your convenience, I am printing it on the screen sir.")
-            print(f"Description: {weather}\nTemperature: {temperature}\nFeels like: {feels_like}")
+        elif "log off" in statement or "sign out" in statement:
+            speak("Logging off all applications...")
+            subprocess.call(["shutdown", "/l"])
+
+            time.sleep(3)
+
+
 
 
 
@@ -134,22 +158,7 @@ if __name__=='__main__':
                   
                   
                   
-        elif 'joke' in query:
-            speak(f"Hope you like this one sir")
-            joke = get_random_joke()
-            speak(joke)
-            speak("For your convenience, I am printing it on the screen sir.")
-            pprint(joke)
-            
-         elif "advice" in query:
-            speak(f"Here's an advice for you, sir")
-            advice = get_random_advice()
-            speak(advice)
-            speak("For your convenience, I am printing it on the screen sir.")
-            pprint(advice)
-                  
-                  
-                  
+      
                   
                   
                   
@@ -159,15 +168,3 @@ if __name__=='__main__':
                   
                   
                   
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-         
- 
-    
-
