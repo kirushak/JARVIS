6fce7c9bea8b851a4ff6d542715e943cc8616a14
Saket Gudimella <106355242+SaketGudimella@users.noreply.github.com>
2022-12-11 09:51:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,65 @@ def takeCommand():
 if __name__=='__main__':
 
 
-    
+    while True:
+        speak("Tell me how can I help you now?")
+        statement = takeCommand().lower()
+        if statement==0:
+            continue
+
+        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
+            speak('Jarvis is shutting down,Good bye')
+            print('Jarvis is shutting down,Good bye')
+            break
+
+            
+            ## Code for opening local applications on host's device (Saket)
+
+ elif 'ip address' in query:
+            ip_address = find_my_ip()
+            speak(f'Your IP Address is {ip_address}.\n For your convenience, I am printing it on the screen sir.')
+            print(f'Your IP Address is {ip_address}')
+
+        elif 'wikipedia' in query:
+            speak('What do you want to search on Wikipedia, sir?')
+            search_query = take_user_input().lower()
+            results = search_on_wikipedia(search_query)
+            speak(f"According to Wikipedia, {results}")
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(results)
+
+        elif 'youtube' in query:
+            speak('What do you want to play on Youtube, sir?')
+            video = take_user_input().lower()
+            play_on_youtube(video)
+
+        elif 'search on google' in query:
+            speak('What do you want to search on Google, sir?')
+            query = take_user_input().lower()
+            search_on_google(query)
+
+        elif "trending movies" in query:
+            speak(f"Some of the trending movies are: {get_trending_movies()}")
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(*get_trending_movies(), sep='\n')
+
+        elif 'news' in query:
+            speak(f"I'm reading out the latest news headlines, sir")
+            speak(get_latest_news())
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(*get_latest_news(), sep='\n')
+
+        elif 'weather' in query:
+            ip_address = find_my_ip()
+            city = requests.get(f"https://ipapi.co/{ip_address}/city/").text
+            speak(f"Getting weather report for your city {city}")
+            weather, temperature, feels_like = get_weather_report(city)
+            speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
+            speak(f"Also, the weather report talks about {weather}")
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(f"Description: {weather}\nTemperature: {temperature}\nFeels like: {feels_like}")
+
+
 
          ## Whatsapp Messaging Feature (Kirushak)
                   
@@ -76,6 +134,19 @@ if __name__=='__main__':
                   
                   
                   
+        elif 'joke' in query:
+            speak(f"Hope you like this one sir")
+            joke = get_random_joke()
+            speak(joke)
+            speak("For your convenience, I am printing it on the screen sir.")
+            pprint(joke)
+            
+         elif "advice" in query:
+            speak(f"Here's an advice for you, sir")
+            advice = get_random_advice()
+            speak(advice)
+            speak("For your convenience, I am printing it on the screen sir.")
+            pprint(advice)
                   
                   
                   
@@ -83,7 +154,7 @@ if __name__=='__main__':
                   
                   
                   
-                  
+          
                   
                   
                   
