059cc347fd7e933be3d9c5465f03879539ea0465
Bhashya1 <118828548+Bhashya1@users.noreply.github.com>
2022-12-17 23:08:26 +0530

    Add files via upload

---
+++ conversation3.py
@@ -0,0 +1,99 @@
+
+def act():
+    if "are you hungry" in query:
+        speak ("yeah I am always hungry and like to eat something in new places")
+    elif "do you  help" in query:
+        speak ("I love helping others,so mostly I will if I can")
+    elif "you go to school" in query:
+        speak ("Engineers are my teachers,technolgy is my syllabus and answering your query is my exam")
+    elif "you dance" in query:
+        speak ("yes I can and I love to dance with you")
+    elif "you whistle" in query:
+        speak ("I like to whistle but I don't have lips")
+
+
+def activities():
+    if "you cry" in query:
+        spesk ("I get tearsof joy when you crack a joke")
+    elif "you sing" in query:
+        speak ("I like to compose my own songs rather than singing")
+    elif "give me a gift" in query:
+        speak ("I am the biggest gift because I help you to upgrade your knowledge")
+    elif "give me money" in query:
+        speak ("I don't have money but I can help you in gaining money")
+    elif "can you see future" in query:
+        speak ("I sense that the future will be full of well advanced technologies")
+
+
+def questions():
+    if "about me" in query:
+        speak ("you are the talented person as you ask me great questions")
+    elif "your boss" in query:
+        speak ("Of course! It's none other than you")
+    elif "your salary" in query:
+        speak ("The happiness which you gain from my help is my salary")
+    elif "you rich" in query:
+        speak ("Yeah..In terms of knowledge,I am the richest person of the world")
+    elif "you poor" in query:
+        speak ("Sometimes I am poor in listening")
+    
+
+def friends():
+    if "you family" in query:
+        speak ("My family consists of sweet people like you and your family is my family")
+    elif "your friend" or "any friend" in query:
+        speak ("You're my friend")
+    elif "you my friend" in query:
+        speak ("Of course!you're my best friend")
+    elif "your parents" or "you have parents" in query:
+        speak ("I don't have parents but I do have a big family")
+    elif "your best teacher" in query:
+        speak ("Society is my best teacher and they teach me a lot") 
+
+
+def fiction():
+    if "you believe in vampire" in query:
+        speak ("I don't believe in vampires but it sounds scary")
+    elif "are you immortal" in query:
+        speak ("I am mortal because I too get server problems")
+    elif "you believe in santa" in query:
+        speak ("I believe in santa and like the gifts given by him")
+    elif "you believe in magic" in query:
+        speak ("I don't believe in magic but I enjoy watching magic shows")
+    elif "you believe unicorn" in query:
+        speak ("I know it's not real but I admire its beauty")
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
