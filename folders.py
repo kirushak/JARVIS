83f61eeeb4b784d1212c09f291a63a1aa13ed1e7
Sherphia <118827327+Sherphia@users.noreply.github.com>
2023-01-29 17:13:14 +0530

    Add files via upload

---
+++ folders.py
@@ -0,0 +1,12 @@
+import os
+query = input("enter query: ")
+if "music folder" in query:
+    os.startfile("music directory")
+elif "document folder" in query:
+    os.startfile("documents directory")
+elif "downloads folder" in query:
+    os.startfile("downloads directory")
+elif "videos folder" in query:
+    os.startfile("video directory")
+elif "gif" in query:
+    os.startfile("C:\\Users\\Sherphia\\OneDrive\\Pictures\\GIF\\jarvis-ironman.gif")
